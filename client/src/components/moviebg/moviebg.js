caa9abddef673d0bb977a314be44a3798272c80a
oliveira002 <deadzin112@gmail.com>
2023-01-26 21:05:50 +0000

    Continue on movie page

--- client/src/components/moviebg/moviebg.js
+++ client/src/components/moviebg/moviebg.js
@@ -3,18 +3,24 @@ import '../banner/banner.css'
 import {faArrowRight, faStar, faPlay} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-export const MovieBg = ({backdrop_path, original_title, overview, release_date,genres,runtime,tagline,poster_path}) => {
+export const MovieBg = ({backdrop_path, original_title, overview, release_date,genres,runtime,tagline,poster_path,vote_average}) => {
     const bannerUrl = `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${backdrop_path}`
     const posterUrl = `https://www.themoviedb.org/t/p/w220_and_h330_face${poster_path}`
     const data = release_date && release_date.slice(0,4);
     let hours = runtime && Math.floor(runtime / 60);
     let remainder = runtime && runtime  % 60;
     let minutesString = remainder < 10 ? "0" + remainder : remainder;
+    let rating = vote_average && vote_average.toFixed(1);
     return(
         <div className="d-flex flex-column">
             <div className="movieBanner mt-4">
                 <div className="">
-                    <img className="posterpic" src={posterUrl} width="220" height="330"/>
+                    <div className="d-flex flex-column posterpic">
+                        <div className="ratingcircle d-flex">
+                            <span className="posterRate fw-bold">{rating}</span>
+                        </div>
+                        <img className="" src={posterUrl} width="250" height="380"/>
+                    </div>
                     <div className="d-flex flex-column overText">
                         <div className="d-flex titulo">
                             <span className="title fw-bold h5">{original_title} ({data})</span>
@@ -45,8 +51,8 @@ export const MovieBg = ({backdrop_path, original_title, overview, release_date,g
                             <span>{overview}</span>
                         </div>
                         <div className="d-flex mt-2">
-                            <button className="check2"> Play Trailer <FontAwesomeIcon className="ms-1" icon={faPlay}/></button>
-                            <button className="favbutton ms-2"> Add to favourites<FontAwesomeIcon className="ms-1" icon={faStar}/></button>
+                            <button className="check2 fw-bold">  <FontAwesomeIcon className="ms-1" icon={faPlay}/> Trailer </button>
+                            <button className="favbutton ms-2 fw-bold"> Add to favourites<FontAwesomeIcon className="ms-1" icon={faStar}/></button>
                         </div>
                     </div>
                 </div>
